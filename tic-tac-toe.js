47806075c34f75f0bda1905ad5c6ee2b6505c856
damullings17 <daniellemullings17@gmail.com>
2020-10-09 17:41:33 -0500

    Exercise 2 - Add an X or O when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,58 @@
+const playerX = "X";
+const playerO = "O";
+var xTurn = true;
+var arrayX = [] ;
+var arrayO = [];
+
+const Wins = 
+[
+    [0,3,6],
+    [0,1,2],
+    [0,4,8],
+    [2,5,8],
+    [0,3,6],
+    [1,4,7],
+    [3,4,5],
+    [6,7,8],
+    [2,4,6]
+]
+
 document.addEventListener("DOMContentLoaded",function()
 {
-    const squares = document.getElementById("board").children;
+    //When the page loads, initialize the array to store the values of where X and Os have been placed
+    
+    
+    array = [];
+    var squares = document.getElementById("board").children; //Get all the children of the board. These are the divs
     for (i = 0; i < squares.length; i++)
     {
-        squares[i].className += "square";
+        squares[i].className += "square"; //Style the squares
+        squares[i].addEventListener('click',clickFunction,{once : true}); //Add an onclick event listener to each of the squares
+        squares[i].id += i; //Add a number ID to each square so that we can check wins
     }
 
+    
+
 })
+
+function clickFunction(e)
+{
+    var currentPlayer = xTurn ? playerX : playerO;
+    
+    const square = e.target;
+    square.innerHTML = square.innerHTML + currentPlayer;
+    square.className = square.className + " " + currentPlayer;
+    
+    //Make a note of the squares that have been played in
+    if (xTurn)
+    {
+        arrayX.push(square.id) ;
+    }
+    else
+    {
+        arrayO.push(square.id) ;
+    }
+
+    xTurn = xTurn ? false : true;
+    
+}
